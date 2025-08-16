0127681fa3979d6870548af58178b0315433ffcf
Avisek Ray <biisal.int@gmail.com>
2025-08-16 15:20:28 +0530

    Refactor install script to handle Linux, Mac, and Windows better

--- install.sh
+++ install.sh
@@ -14,15 +14,19 @@ DIR=""
 URL=""
 GREEN='\033[0;32m'
 NC='\033[0m'
-if [ "$os" == "linux" ] || [ "$os" == "mac" ]; then
+if [ "$os" != "windows" ] ; then
 	DIR="/usr/local/bin/"
 	URL="https://github.com/biisal/tipp/releases/latest/download/tipp"
-elif [ "$os" == "windows" ]; then
+	if [ ! -d "$DIR" ]; then
+		echo -e "Creating $DIR"
+		sudo mkdir -p "$DIR"
+	fi
+else
 	DIR="$HOME/bin/"
 	URL="https://github.com/biisal/tipp/releases/latest/download/tipp.exe"
+	mkdir -p "$DIR"
 fi
 
-mkdir -p "$DIR"
 
 echo -e "${GREEN}Downloading ${NC} tipp to $DIR"
 sudo curl -L "$URL" -o "${DIR}tipp"
